09cc4cd55e7f38590edcd5c89a613493532932f5
userMonkNik <normas24@mail.ru>
2022-11-08 01:13:57 +0300

    spring boot app, gradle buid tool, add account entity with validation annotations, auth controller

---
+++ src/main/java/accountService/Account.java
@@ -0,0 +1,51 @@
+package accountService;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+public class Account {
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    private String lastname;
+    @Email(regexp = ".+(@acme.com)")
+    private String email;
+    @NotEmpty
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
